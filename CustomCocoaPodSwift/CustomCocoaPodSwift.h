f1bc6302587254143f8a4eb49641bc13715b824d
Vijay Bhaskar <kemisettykalyani308@gmail.com>
2023-11-13 13:30:30 +0530

    Initial Commit

---
+++ CustomCocoaPodSwift/CustomCocoaPodSwift.h
@@ -0,0 +1,18 @@
+//
+//  CustomCocoaPodSwift.h
+//  CustomCocoaPodSwift
+//
+//  Created by Karthik_Ruthvik on 13/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomCocoaPodSwift.
+FOUNDATION_EXPORT double CustomCocoaPodSwiftVersionNumber;
+
+//! Project version string for CustomCocoaPodSwift.
+FOUNDATION_EXPORT const unsigned char CustomCocoaPodSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomCocoaPodSwift/PublicHeader.h>
+
+
